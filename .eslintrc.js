94f3015e8bb0644739fade05f5b8c4879bcfb68f
Andrew McGill <arm5077@gmail.com>
2018-04-24 13:53:13 -0400

    Update no-unused-vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   "extends": "airbnb-base",
   "rules": {
     "no-unused-vars": ["warn", { "vars": "local", "args": "after-used", "ignoreRestSiblings": false }],
-    'prefer-arrow-callback': ['warn']
+    'prefer-arrow-callback': ['warn'],
+    'no-unused-vars': ['warn']
   },
   "env": {
     "browser": true
